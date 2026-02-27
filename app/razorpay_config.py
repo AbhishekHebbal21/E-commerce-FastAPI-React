189d192874102f71a05d0232f53854e765512b36
AbhishekHebbal21 <sabhishek.hebbal@gmail.com>
2026-02-27 15:00:34 +0530

    Added basic payment method

--- app/razorpay_config.py
+++ app/razorpay_config.py
@@ -1,10 +1,10 @@
-# import os
-# from dotenv import load_dotenv
-# import razorpay
+import os
+from dotenv import load_dotenv
+import razorpay
 
-# load_dotenv()
+load_dotenv()
 
-# RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
-# RAZORPAY_SECRET_KEY = os.getenv("RAZORPAY_SECRET_KEY")
+RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
+RAZORPAY_SECRET_KEY = os.getenv("RAZORPAY_SECRET_KEY")
 
-# razorpay_client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_SECRET_KEY))
+razorpay_client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_SECRET_KEY))
